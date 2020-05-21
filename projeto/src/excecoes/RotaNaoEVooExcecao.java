f55ccdf9489af31feb029647fd7aedefa06d26ca
Guicbdiniz <guicbdeniz@hotmail.com>
2020-05-21 14:14:49 -0300

    feat: add custom exception class RotaNaoEVooExcecao

---
+++ projeto/src/excecoes/RotaNaoEVooExcecao.java
@@ -0,0 +1,11 @@
+package excecoes;
+
+public class RotaNaoEVooExcecao extends Exception {
+
+    public RotaNaoEVooExcecao(String message) {
+        super(message);
+    }
+
+    private static final long serialVersionUID = 1L;
+
+}
