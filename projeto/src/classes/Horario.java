f6c2bdcddeb4df010fa6b4529fe6408a284ce72b
Guicbdiniz <guicbdeniz@hotmail.com>
2020-05-21 20:26:23 -0300

    feat: add equals override in the Horario class

--- projeto/src/classes/Horario.java
+++ projeto/src/classes/Horario.java
@@ -24,4 +24,41 @@ public class Horario {
     public int pegaMinutos() {
         return minutos;
     }
+
+    /**
+     * Sobrecarga no método de comparação para que testes possam ser feitos.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (!Horario.class.isAssignableFrom(obj.getClass())) {
+            return false;
+        }
+
+        final Horario outro = (Horario) obj;
+        if (outro.pegaHoras() != this.horas) {
+            return false;
+        }
+
+        if (outro.pegaMinutos() != this.minutos) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Sobrecarga no método de criação de código hash para que testes possam ser
+     * feitos.
+     */
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + this.horas;
+        hash = 53 * hash + this.minutos;
+        return hash;
+    }
 }
