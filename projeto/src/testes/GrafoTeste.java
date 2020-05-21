e16b0043819d009190d04892273c9a6886d86d40
Guicbdiniz <guicbdeniz@hotmail.com>
2020-05-21 13:13:46 -0300

    test: add class Grafo tests

--- projeto/src/testes/GrafoTeste.java
+++ projeto/src/testes/GrafoTeste.java
@@ -1,8 +1,36 @@
 package testes;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import classes.Grafo;
+
 /**
  * Testes para a classe Grafo.
  */
 public class GrafoTeste {
+
+    private Grafo grafoTeste;
+
+    @Before
+    public void setUp() {
+        this.grafoTeste = new Grafo();
+    }
+
+    @Test
+    public void testaAdicionaVertice() {
+        this.grafoTeste.adicionaVertice("A");
+
+        assertEquals(1, this.grafoTeste.pegaNumeroDeVertices());
+    }
+
+    @Test
+    public void testaAdicionaAresta() {
+        this.grafoTeste.adicionaAresta("A", "B", 2);
+
+        assertEquals(2, this.grafoTeste.pegaNumeroDeVertices());
+    }
 
 }
