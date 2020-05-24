83d2a8dbabbec8ad1ea49f9ca99155e89891ca49
Guicbdiniz <guicbdeniz@hotmail.com>
2020-05-23 21:45:58 -0300

    feat: add class Caminho

---
+++ projeto/src/classes/Caminho.java
@@ -0,0 +1,65 @@
+package classes;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * Caminho entre vértices em um grafo.
+ */
+public class Caminho {
+
+    private Queue<Vertice> sequenciaDeVertices;
+
+    /** Simples construtor */
+    public Caminho(Queue<Vertice> sequenciaDeVertices) {
+        this.sequenciaDeVertices = sequenciaDeVertices;
+    }
+
+    /**
+     * Pega um caminho a partir de uma pilha de vertices, considerando que o
+     * primeiro que foi inserido será o vértice incial do caminho (e a ordem
+     * seguirá).
+     * 
+     * @param pilha - pilha de vértices com a ordem do caminho.
+     * @return caminho gerado a partir da pilha.
+     */
+    public static Caminho pegaCaminhoDePilha(Stack<Vertice> pilha) {
+
+        Stack<Vertice> pilhaInicial = new Stack<Vertice>();
+        pilhaInicial.addAll(pilha);
+
+        Stack<Vertice> pilhaExtra = new Stack<Vertice>();
+        while (!pilhaInicial.isEmpty()) {
+            pilhaExtra.add(pilhaInicial.pop());
+        }
+
+        Queue<Vertice> sequenciaDeVertices = new LinkedList<Vertice>();
+        while (!pilhaExtra.isEmpty()) {
+            sequenciaDeVertices.add(pilhaExtra.pop());
+        }
+
+        return new Caminho(sequenciaDeVertices);
+    }
+
+    /** Pega sequência de vértices do caminho. */
+    public Queue<Vertice> pegaSequenciaDeVertices() {
+        return this.sequenciaDeVertices;
+    }
+
+    /**
+     * Pega sequencia de IDs dos vértices do caminho.
+     * 
+     * @return sequência de IDs dos vértices.
+     */
+    public Queue<String> pegaSequenciaDeIDDeVertices() {
+        Queue<Vertice> filaDeVerticesAdicional = new LinkedList<Vertice>(sequenciaDeVertices);
+        Queue<String> sequenciaDeIDDeVertices = new LinkedList<String>();
+        while (!filaDeVerticesAdicional.isEmpty()) {
+            sequenciaDeIDDeVertices.add(filaDeVerticesAdicional.remove().pegaID());
+        }
+
+        return sequenciaDeIDDeVertices;
+    }
+
+}
